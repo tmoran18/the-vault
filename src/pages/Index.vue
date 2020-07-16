dba2ccec2787cad4f8cceb32f45ffece0c713772
Tim Moran <tim.moran@conversify.com.au>
2020-07-16 12:32:19 +1000

    Started to add resource links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,33 @@
 <template>
-  <Layout> </Layout>
+  <Layout>
+    <h1>Resources</h1>
+    <div>
+      <h2>Career</h2>
+      <a href="https://30secondsofinterviews.org/">30 Seconds of Interviews</a>
+      <h2>CSS Layout</h2>
+      <a href="https://every-layout.dev/">CSS Layouts - Every Layout</a>
+      <a href="http://apps.eky.hk/css-triangle-generator/"
+        >CSS Triangle Generator</a
+      >
+      <a href="https://bennettfeely.com/clippy/"
+        >Clip Path Generator - Clippy</a
+      >
+      <a href="https://brumm.af/shadows">CSS Box Shadow Generator - Brumm</a>
+      <h2>Images/Icons</h2>
+      <a href="https://www.flaticon.com/">Flat Icon</a>
+      <h2>Utilities</h2>
+      <a href="https://necolas.github.io/normalize.css/">Normalize CSS</a>
+      <a href="https://www.lambdatest.com/">Browser Testing - Lambda Test</a>
+      <h2>Font</h2>
+      <a href="https://fontjoy.com/">Font Pairing - Font Joy</a>
+    </div>
+  </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: "Hello, world!",
+    title: "Vault Docs",
   },
 };
 </script>
